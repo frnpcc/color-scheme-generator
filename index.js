7a07411895e9902c4030c1b097e406729fa52936
Francesca Correa <francescacciavaglia@gmail.com>
2024-04-11 19:06:24 +0200

    add event listener to form to collect inputs and fetch specific data & function call to render color schema

---
+++ index.js
@@ -0,0 +1,33 @@
+document.getElementById('select-color').addEventListener('submit', function(e){
+
+  e.preventDefault()
+
+  let url= 'https://www.thecolorapi.com'
+  const colorHex = document.getElementById('color-hex').value
+  const mode = document.getElementById('mode-options').value
+  url += `/scheme?hex=${colorHex.substring(1)}&mode=${mode}&count=5`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+
+      let colorsHtml = ''
+
+      data.colors.forEach((color, index) => colorsHtml += `
+      <img
+      id="color-${index + 1}"
+      src="${color.image.bare}"
+      alt="image for color ${color.name.value}">
+      <span id="hex-${index + 1}">${color.hex.value}</span>
+      `)
+
+      renderColorScheme(colorsHtml)
+    })
+
+})
+
+function renderColorScheme(html) {
+
+  document.getElementById('colors-container').innerHTML = html
+
+}
